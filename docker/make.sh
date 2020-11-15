d18b42d4a6e825889cd9578326d0a7ef50c0da68
Joey Hou <z9hou@ucsd.edu>
2020-11-14 17:27:39 -0800

    updated dockerfile and make.sh

--- docker/make.sh
+++ docker/make.sh
@@ -2,14 +2,18 @@
 # archive file to the Dockerfile--we can create an archive here instead, and
 # pass that in, but in Windows, scripts in such an archive end up with Windows
 # linefeeds under typical git setings, and won't execute properly in Linux.
-if REPO_URL=$(git remote get-url origin) 2> /dev/null; then
-    REPO=$(echo $REPO_URL | sed "s/\.git//")
-    BRANCH=$(git rev-parse --abbrev-ref HEAD)
-else
-    # REPO=https://github.com/shangjingbo1226/AutoPhrase
-    REPO=https://github.com/JoeyHou/DSC180A_Replication
-    BRANCH=master
-fi
+# if REPO_URL=$(git remote get-url origin) 2> /dev/null; then
+#     REPO=$(echo $REPO_URL | sed "s/\.git//")
+#     BRANCH=$(git rev-parse --abbrev-ref HEAD)
+# else
+#     # REPO=https://github.com/shangjingbo1226/AutoPhrase
+#     REPO=https://github.com/JoeyHou/DSC180A_Replication
+#     BRANCH=master
+# fi
+
+REPO=https://github.com/JoeyHou/DSC180A_Replication
+BRANCH=master
+
 echo $REPO
 # In a Windows bash shell, "sudo" won't work.  Note that the shell itself
 # must be elevated, though.
