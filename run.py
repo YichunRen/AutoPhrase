a5b5718e34b8cd7b956cbca5ee87e41c2fc2d93e
Joey Hou <z9hou@ucsd.edu>
2020-11-25 15:50:46 -0800

    updated test file location in run.py

--- run.py
+++ run.py
@@ -120,7 +120,7 @@ def autophrase(runtime_status):
 
     if runtime_status['testing'] == 1:
         print(" => Running test data...")
-        method_params['RAW_TRAIN'] = "data/EN/test_raw.txt"
+        method_params['RAW_TRAIN'] = "test/testdata/test_raw.txt"
 
     command = 'cp src/run_phrasing.sh . ; ./run_phrasing.sh '
     for key in method_params.keys():
